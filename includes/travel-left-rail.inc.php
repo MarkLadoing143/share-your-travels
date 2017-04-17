edc585eecd392c37393c067131e2971838e3bec9
Mark Ladoing <MarkLadoing143@users.noreply.github.com>
2017-04-16 21:22:04 -0600

    Add files via upload

---
+++ includes/travel-left-rail.inc.php
@@ -0,0 +1,68 @@
+<?php
+require_once('includes/travel-config.php');
+
+function outputContinents() {
+	try {
+		$pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		$sql = "SELECT ContinentName FROM geocontinents GROUP BY ContinentName";
+		$result = $pdo->query($sql);
+		while( $row = $result->fetch() ) {
+			echo "<li class='list-group-item'><a href=#>" . $row['ContinentName'] . "</a></li>";
+		}
+		$pdo = null;
+	}
+	catch (PDOException $e) {
+		die( $e->getMessage() );
+	}
+}
+
+function outputPopCountries() {
+	try {
+		$pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		$sql = "SELECT DISTINCT CountryName, geocountries.ISO FROM geocountries INNER JOIN travelimagedetails ON geocountries.ISO = travelimagedetails.CountryCodeISO ORDER BY CountryName";
+		$result = $pdo->query($sql);
+		while( $row = $result->fetch() ) {
+			echo "<li class='list-group-item'><a href='single-country.php?iso=" . $row['ISO'] . "'>" . $row['CountryName'] . "</a></li>";
+		}
+		$pdo = null;
+	}
+	catch (PDOException $e) {
+		die( $e->getMessage() );
+	}
+}
+
+?>         
+		 
+		 <div class="panel panel-default">
+           <div class="panel-heading">Search</div>
+           <div class="panel-body">
+             <form>
+               <div class="input-group">
+                  <input type="text" class="form-control" placeholder="search ...">
+                  <span class="input-group-btn">
+                    <button class="btn btn-warning" type="button"><span class="glyphicon glyphicon-search"></span>          
+                    </button>
+                  </span>
+               </div>  
+             </form>
+           </div>
+         </div>  <!-- end search panel -->       
+      
+         <div class="panel panel-info">
+           <div class="panel-heading">Continents</div>
+           <ul class="list-group">   
+		    <?php outputContinents(); ?>
+
+           </ul>
+         </div>  <!-- end continents panel -->  
+         <div class="panel panel-info">
+           <div class="panel-heading">Popular Countries</div>
+           <ul class="list-group">  
+		    <?php outputPopCountries(); ?>
+  
+           </ul>
+         </div>  <!-- end countries panel -->    
